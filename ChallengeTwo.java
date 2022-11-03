469e4bd0ac33dd90d478458070309c288727db1e
shaden-s <74512014+shaden-s@users.noreply.github.com>
2022-11-03 10:44:26 +0000

    Create ChallengeTwo.java

---
+++ ChallengeTwo.java
@@ -0,0 +1,10 @@
+public class ChallengeTwo {
+    public static void main(String[] args){
+        int total = 0;
+        for(int i=0; i<11;i++){
+            total += i;
+        }
+        System.out.println(total);
+
+    }
+}
